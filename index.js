77e481a2e6e94b106c719c7be8cb3d6dde1f0b79
Paul Aliu <paul.iyola.aliu@gmail.com>
2019-05-01 11:55:49 +0100

    managed to refactor code and have process run without rp

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 require('dotenv').config()
-const rp = require('request-promise')
 const twilio = require('twilio')
+const translate = require('yandex-translate')(process.env.YANDEX_TRANSLATE_KEY)
+const client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_SECRET_KEY)
 
-rp({
-  method: 'POST',
-  url: 'https://translate.yandex.net/api/v1.5/tr.json/translate',
-  'json': true,
-  'qs': {
-    key: process.env.YANDEX_TRANSLATE_KEY,
-    text: 'Hello',
-    lang: 'de'
-  }
-})
-  .then(res => {
-    const client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_SECRET_KEY)
-    client.messages.create({
-      body: res.text,
-      to: '+447496045540',
-      from: '+447723444712'
-    })
-      .then(message => console.log(message.sid))
+translate.translate('Hello', { to: 'ru' }, function(err, res){
+  client.messages.create({
+    body: res.text,
+    to: '+447496045540',
+    from: '+447723444712'
   })
+    .then(message => console.log(message.sid))
+})
