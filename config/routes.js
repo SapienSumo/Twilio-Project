580389f9fd0ed9aba22fd7a4dd1269a0e2c769e9
Paul Aliu <paul.iyola.aliu@gmail.com>
2019-05-01 14:31:09 +0100

    Lab completed, and refactoring done. Removed extra dirs and used only index.js

--- config/routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const textController = require('../controllers/texts')
-
-router.get('/', (req, res) => res.json({ message: 'Welcome to our text translator!'}))
-router.post('/sendtext', textController.sendText)
-
-module.exports = router
--- index.js
+++ index.js
@@ -1,13 +1,33 @@
+require('dotenv').config()
+
 const express = require('express')
 const mongoose = require('mongoose')
-const routes = require('./config/routes')
 const bodyParser = require('body-parser')
+
+const twilio = require('twilio')
+const translate = require('yandex-translate')(process.env.YANDEX_TRANSLATE_KEY)
+const client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_SECRET_KEY)
 
 const app = express()
 
 mongoose.connect('mongodb://localhost:27017/twilio-translate')
 
 app.use(bodyParser.json())
-app.use(routes)
 
-app.listen(4000, () => console.log('Translating on port 4K'))
+app.get('/', (req, res) => res.json({ message: 'Welcome to our text translator!'}))
+app.post('/message', (req, res) => {
+  const message = req.body.message
+  const language = req.body.lang
+
+  translate.translate(message, { to: language }, function(err, response){
+    return client.messages.create({
+      body: response.text,
+      to: req.body.toNumber,
+      from: req.body.fromNumber
+    })
+      .then(() => res.json({ message: 'Translation successful. Message sent.' }))
+  })
+
+})
+
+app.listen(4000, () => console.log('Translating on port 4000'))
