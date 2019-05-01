f534833bfb31ea671513c26fe5d1900218ea466d
Paul Aliu <paul.iyola.aliu@gmail.com>
2019-05-01 11:53:56 +0100

    Got messaging system to translate and send with rp (twilio/yandex)

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-// require ('dotenv').config()
-
 require('dotenv').config()
-// const rp = require('request-promise')
+const rp = require('request-promise')
 const twilio = require('twilio')
 
-const client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_SECRET_KEY)
-
-client.messages.create({
-  body: 'Hi',
-  to: '+447496045540',
-  from: '+447723444712'
+rp({
+  method: 'POST',
+  url: 'https://translate.yandex.net/api/v1.5/tr.json/translate',
+  'json': true,
+  'qs': {
+    key: process.env.YANDEX_TRANSLATE_KEY,
+    text: 'Hello',
+    lang: 'de'
+  }
 })
-  .then(message => console.log(message.sid))
-
-
-
-// rp({
-// method: 'GET',
-// url: `https:///forecast/${process.env.DARKSKY_SECRET_KEY}/37.8267,-122.4233`,
-// json: true
-// })
-// .then(res => console.log(res))
+  .then(res => {
+    const client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_SECRET_KEY)
+    client.messages.create({
+      body: res.text,
+      to: '+447496045540',
+      from: '+447723444712'
+    })
+      .then(message => console.log(message.sid))
+  })
